a1fff51992af9e644c04dcbc79f5a28fd16a738c
Bobrik V. Oleg <olegvbobrik@gmail.com>
2019-10-02 20:25:31 +0500

    Add items form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,15 +3,38 @@ import React, { Component } from 'react'
 import './item-add-form.css';
 
 class ItemAddForm extends Component {
+
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value.toUpperCase()
+    });
+  };
+
+  onSubmit = (e) => {
+    if (this.state.label) {
+      this.props.onItemAdded(this.state.label);
+      this.setState({
+        label: ''
+      });
+    }
+
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <div className="item-add-form">
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input type="text" className="item-add-input from-control" onChange={this.onLabelChange} placeholder="What needs to be done"
+        value={this.state.label}/>
         <button
-          className='btn btn-outline-primary btn-block'
-          onClick={() => this.props.onItemAdded('Hello!')}>
+          className='item-add-btn btn btn-outline-primary btn-block'>
           Add Item
         </button>
-      </div>
+      </form>
     );
   }
 }
